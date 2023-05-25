c2f528bc8e946aea027c50d05bbff419d3f57919
Davranbek <davranbek_1995@mail.ru>
2023-05-25 20:53:04 +0500

    added maket

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+// main slides
+
+$('.popular-items__cards').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,  // бесконечный цикл
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 800,
+    
+});
+
+
+
+$('.foods-cards').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,  // бесконечный цикл
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 800,
+    
+});
+
